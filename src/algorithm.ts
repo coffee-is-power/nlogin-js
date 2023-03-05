7dd6562767d26ae13f7601a527ce32ea1b3aba70
tiago <tiagodinis33@gmail.com>
2023-03-05 01:06:48 +0000

    fix: converted to typescript and done some maintainance

---
+++ src/algorithm.ts
@@ -0,0 +1,154 @@
+import bcrypt from "bcryptjs";
+import crypto from "node:crypto";
+import uniqid from "uniqid";
+/*
+Generating random numbers in specific range using crypto.randomBytes from crypto library
+Maximum available range is 281474976710655 or 256^6-1
+Maximum number for range must be equal or less than Number.MAX_SAFE_INTEGER (usually 9007199254740991)
+Usage examples:
+cryptoRandomNumber(0, 350);
+cryptoRandomNumber(556, 1250425);
+cryptoRandomNumber(0, 281474976710655);
+cryptoRandomNumber((Number.MAX_SAFE_INTEGER-281474976710655), Number.MAX_SAFE_INTEGER);
+
+Tested and working on 64bit Windows and Unix operation systems.
+*/
+function cryptoRandomNumber(min: number | string = 0, max: number | string = 2147483647) {
+    if (typeof min == "string")
+        min = parseInt(min, 10)
+    if (typeof max == "string")
+        max = parseInt(max, 10)
+    let distance = max - min;
+    if (min >= max) {
+        throw new Error('Warning: cryptoRandomNumber() minimum number should be less than maximum');
+    } else if (distance > 281474976710655) {
+        throw new Error('Warning: cryptoRandomNumber() range is greater than 256^6-1');
+    } else if (max > Number.MAX_SAFE_INTEGER) {
+        throw new Error('Warning: cryptoRandomNumber() maximum number should be safe integer limit');
+    } else {
+        let maxBytes = 6;
+        let maxDec = 281474976710656;
+
+        // To avoid huge mathematical operations and increase function performance for small ranges
+        if (distance < 256) {
+            maxBytes = 1;
+            maxDec = 256;
+        } else if (distance < 65536) {
+            maxBytes = 2;
+            maxDec = 65536;
+        } else if (distance < 16777216) {
+            maxBytes = 3;
+            maxDec = 16777216;
+        } else if (distance < 4294967296) {
+            maxBytes = 4;
+            maxDec = 4294967296;
+        } else if (distance < 1099511627776) {
+            maxBytes = 4;
+            maxDec = 1099511627776;
+        }
+
+        let randbytes = parseInt(crypto.randomBytes(maxBytes).toString('hex'), 16);
+        let result = Math.floor(randbytes / maxDec * (max - min + 1) + min);
+
+        if (result > max) {
+            result = max;
+        }
+        return result;
+    }
+}
+
+interface Algorithm {
+    hash(passwd: string): string;
+    isValid(passwd: string, hash: string): boolean;
+}
+const SALT_LENGTH = 16;
+function hash(method: string, passwd: string) {
+    return crypto.createHash(method).update(passwd).digest('hex')
+}
+function sha256(passwd: string) {
+    return hash("sha256", passwd);
+}
+function crypt(passwd: string, salt: string) {
+    return bcrypt.hashSync(passwd, salt);
+}
+const SALT_PREFIX = "2a";
+const DEFAULT_COST = 14;
+const BCRYPT_SALT_LENGTH = 22;
+function base64_encode(str: string) {
+    return Buffer.from(str, "base64").toString();
+}
+class BCrypt implements Algorithm {
+    generateRandomSalt() {
+        let salt = crypto.randomBytes(11).toString("hex");
+        return salt.substring(0, salt.length);
+    }
+    hash(passwd: string, cost: number = DEFAULT_COST): string {
+        const salt = this.generateRandomSalt()
+        const hashString = this.generateHashString(cost, salt)
+        return crypt(passwd, hashString)
+
+    }
+    isValid(passwd: string, hash: string): boolean {
+        return crypt(passwd, hash) == hash
+    }
+    generateHashString(cost: number, salt: String) {
+        return "$" + SALT_PREFIX + "$" + String(cost).padStart(2, '0') + "$" + salt + "$";
+    }
+}
+const AUTHME_CHARS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+class AuthMe implements Algorithm {
+    hash(passwd: string) {
+        let salt = this.generateSalt()
+        return `$SHA$${salt}$${sha256(sha256(passwd) + salt)}$AUTHME`
+    }
+
+    isValid(passwd: string, hash: string) {
+        const parts = hash.split("$")
+        const count = parts.length
+
+        return (count == 4 || count == 5) && parts[3] == sha256(sha256(passwd) + parts[2])
+    }
+    generateSalt() {
+        const maxCharIndex = 15;
+        let salt = "";
+        for (let i = 0; i < SALT_LENGTH; i++) {
+            salt += AUTHME_CHARS[cryptoRandomNumber(0, maxCharIndex)]
+        }
+        return salt;
+    }
+}
+const SHA256_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const SHA256_SALT_LENGTH = 24
+class SHA256 implements Algorithm {
+    hash(passwd: string) {
+        const salt = this.generateSalt()
+        return `$SHA256$${sha256(sha256(passwd) + salt)}$${salt}`
+    }
+    generateSalt() {
+        const maxCharIndex = SHA256_CHARS.length - 1
+        let salt = "";
+        for (let i = 0; i < SHA256_SALT_LENGTH; i++) {
+            salt += SHA256_CHARS[cryptoRandomNumber(0, maxCharIndex)]
+        }
+        return salt
+    }
+    isValid(passwd: string, hash: string) {
+        let parts = hash.split("$")
+        switch (parts.length) {
+            case 3:
+                let saltParts = hash.split("@");
+                let salt = saltParts[1];
+                return parts[2] + "@" + salt == sha256(sha256(passwd) + salt)
+            case 4:
+                return parts[2] == sha256(sha256(passwd) + parts[3])
+            default:
+                return false
+        }
+    }
+}
+export {
+    Algorithm,
+    AuthMe,
+    BCrypt,
+    SHA256,
+};
---
+++ src/index.ts
@@ -0,0 +1,171 @@
+import mysql from "mysql";
+import { BCrypt, SHA256, AuthMe, Algorithm } from "./algorithm.js"
+
+
+const TABLE_NAME = 'nlogin'
+class nLogin {
+    bcrypt: BCrypt = new BCrypt();
+    sha256: SHA256 = new SHA256();
+    authme: AuthMe = new AuthMe();
+    def_algo: Algorithm = this.bcrypt;
+    pool: mysql.Pool;
+    constructor(options: mysql.PoolConfig) {
+        this.pool = mysql.createPool(options);
+    }
+
+    /**
+     * Retrieves the hash associated with the given user from the database.
+     */
+    getHashedPassword(username: string): Promise<string> {
+        return new Promise((resolve, reject) => {
+            username = username.trim();
+            this.pool.query(`select password from nlogin where name = '${username.toLowerCase()}' limit 1`, (err, result, fields) => {
+                if (err) return reject(err);
+                resolve(result[0] ? result[0].password : "")
+            })
+        })
+    }
+    checkPassword(username: string, password: string): Promise<boolean> {
+        return this.getHashedPassword(username).then((hash) => {
+            if (hash) {
+                let algorithm = this.detectAlgorithm(hash);
+                if (algorithm) {
+                    return algorithm.isValid(password, hash)
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        });
+
+
+    }
+
+    /**
+     * Returns the algorithm used on the hash.
+     */
+    detectAlgorithm(hashed_pass: String) {
+        let algo = (hashed_pass.includes("$") ? hashed_pass.split("$")[1] : '').toUpperCase();
+        switch (algo) {
+            case '2':
+            case '2A':
+                return this.bcrypt;
+
+            case "PBKDF2":
+                // will be added
+                return null;
+
+            case "ARGON2I":
+                // will be added
+                return null;
+
+            case "SHA256":
+                return this.sha256;
+
+            case "SHA":
+                return this.authme;
+
+            default:
+                return null;
+        }
+    }
+    hash(passwd: string) {
+        return this.def_algo.hash(passwd);
+    }
+    async getEmail(username: string): Promise<string> {
+        return new Promise((resolve, reject) => {
+            username = username.trim();
+            this.pool.query(`select email from nlogin where name = ? limit 1`, [username.toLowerCase()], (err, result, fields) => {
+                if (err) return reject(err);
+                resolve(result[0] ? result[0].email : null)
+            })
+        })
+    }
+    async setEmail(username: string, email: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            username = username.trim()
+            this.pool.query("UPDATE nlogin SET email = ? WHERE name = ?", [email, username.toString()], (err, result, fields) => {
+                if (err) return reject(err)
+                resolve()
+            })
+        })
+    }
+    async setIp(username: string, ip: string, callback = null): Promise<void> {
+        return new Promise((resolve, reject) => {
+            username = username.trim()
+            this.pool.query(`UPDATE nlogin SET address = ? WHERE name = ?`, [ip, username.toLowerCase()], (err, result, fields) => {
+                if (err) return reject(err);
+                resolve();
+            })
+        })
+    }
+    async getIp(username: string): Promise<string | null> {
+
+        return new Promise((resolve, reject) => {
+            username = username.trim();
+            this.pool.query(`select address from nlogin where name = ? limit 1`, [username.toLowerCase()], (err, result, fields) => {
+                if (err) return reject(err);
+                resolve(result[0] ? result[0].ip : null)
+            })
+        })
+    }
+    async isUserRegistered(username: string): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            username = username.trim();
+            this.pool.query(`SELECT 1 FROM ${TABLE_NAME} WHERE name = '${username.toLowerCase()}' LIMIT 1`, (err, result, fields) => {
+                if (err) return reject(err);
+                resolve(result.length > 0)
+            });
+        })
+    }
+    async isIpRegistered(address: string): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            this.pool.query('SELECT 1 FROM ? WHERE address = ? LIMIT 1', [TABLE_NAME, address], (err, result, fields) => {
+                if (err) return reject(err);
+                resolve(result.length > 0)
+            });
+        })
+    }
+    /**
+     * Changes password for player.
+     */
+    async changePassword(passwd: string, username: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            username = username.trim()
+            let hash = this.hash(passwd)
+            this.pool.query(`UPDATE nlogin SET password = ? WHERE name = ?`, [hash, username.toLowerCase()], (err, result, fields) => {
+                if (err) return reject(err);
+
+                resolve()
+            })
+
+        })
+    }
+    async getInfo(username: string): Promise<object> {
+        return new Promise((resolve, reject) => {
+            username = username.trim();
+            this.pool.query(`select * from nlogin where name = ? limit 1`, [username.toLowerCase()], (err, result, fields) => {
+                if (err) return reject(err);
+                resolve(result[0]);
+            })
+        })
+    }
+    async register(username: string, password: string, email: string = "", ip: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            username = username.trim();
+            let usernameLowerCase = username.toLowerCase();
+            let hash = this.hash(password);
+            this.pool.query(`insert into ? (name, real_name, address, password, email) values (?, ?, ?, ?, ?)`, [TABLE_NAME, usernameLowerCase, username, ip, hash, email], (err, result, fields) => {
+                if (err) return reject(err);
+                resolve()
+            })
+        })
+    }
+}
+export {
+    nLogin,
+    BCrypt,
+    SHA256,
+    AuthMe
+}
